b1df768dc51f5e3fd519a0e46a106880de10027f
rhian-cs <rhian.luis.cs+github@gmail.com>
2022-03-22 11:16:22 -0300

    Add calculate method to MyString module in Chapter 11

--- book/chapter-11-strings-and-binaries/my_string.exs
+++ book/chapter-11-strings-and-binaries/my_string.exs
@@ -15,4 +15,42 @@ defmodule MyString do
   defp do_anagram?(charlist1, charlist2) do
     Enum.sort(charlist1) == Enum.sort(charlist2)
   end
+
+  def calculate(operation_str) do
+    operation_str |> filter_blank |> parse_operation
+  end
+
+  defp filter_blank(str) do
+    Enum.reject(str, &(&1 == ?\s))
+  end
+
+  defp parse_operation(operation_str) do
+    {charlist1, [operation | charlist2]} = split_operation_str(operation_str)
+
+    calculate_operation(
+      [operation],
+      [
+        integer_from_charlist(charlist1),
+        integer_from_charlist(charlist2)
+      ]
+    )
+  end
+
+  defp split_operation_str(operation_str) do
+    Enum.split_while(operation_str, &(&1 in ?0..?9))
+  end
+
+  defp integer_from_charlist(charlist) do
+    Enum.reverse(charlist) |> do_integer_from_charlist(1)
+  end
+
+  defp do_integer_from_charlist([head | tail], factor) when head in ?0..?9 do
+    do_integer_from_charlist(tail, factor * 10) + (head - ?0) * factor
+  end
+
+  defp do_integer_from_charlist([], _), do: 0
+
+  defp calculate_operation(operation, numbers) do
+    apply(Kernel, operation |> List.to_atom(), numbers)
+  end
 end
