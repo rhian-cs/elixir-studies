cc764815beaa6d0bf66160ed0c0eee486852aec7
rhian-cs <rhian.luis.cs+github@gmail.com>
2022-04-10 15:49:08 -0300

    Add basic CLI module for weather parser

--- book/chapter-13-organizing-a-project/workshop/experiment1/weather_parser/lib/weather_parser/cli.ex
+++ book/chapter-13-organizing-a-project/workshop/experiment1/weather_parser/lib/weather_parser/cli.ex
@@ -1,6 +1,8 @@
 defmodule WeatherParser.CLI do
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args()
+    |> process
   end
 
   def parse_args(argv) do
@@ -17,5 +19,13 @@ defmodule WeatherParser.CLI do
       switches: [help: :boolean],
       aliases: [h: :help]
     )
+  end
+
+  def process(:help) do
+    IO.puts("""
+    usage: weather_parser <location>
+    """)
+
+    System.halt(0)
   end
 end
