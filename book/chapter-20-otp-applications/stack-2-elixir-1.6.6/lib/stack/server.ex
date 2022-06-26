97586cac0846d00b304d601d1419dbfa234143c2
rhian-cs <rhian.luis.cs+github@gmail.com>
2022-06-26 01:53:14 -0300

    Copy stack application to elixir 1.6

---
+++ book/chapter-20-otp-applications/stack-2-elixir-1.6.6/lib/stack/server.ex
@@ -0,0 +1,40 @@
+defmodule Stack.Server do
+  use GenServer
+  alias Stack.Impl
+
+  @vsn "0"
+
+  ## External API
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  def push(new_element) do
+    GenServer.cast(__MODULE__, {:push, new_element})
+  end
+
+  ## GenServer implementation
+
+  def init(_) do
+    {:ok, Stack.Stash.get()}
+  end
+
+  def handle_call(:pop, _from, stack) do
+    {element, new_stack} = Impl.pop(stack)
+
+    {:reply, element, new_stack}
+  end
+
+  def handle_cast({:push, new_element}, stack) do
+    {:noreply, Impl.push(stack, new_element)}
+  end
+
+  def terminate(_reason, stack) do
+    Stack.Stash.update(stack)
+  end
+end
---
+++ book/chapter-20-otp-applications/stack-2-elixir-1.6.6/lib/stack/stash.ex
@@ -0,0 +1,31 @@
+defmodule Stack.Stash do
+  use GenServer
+
+  @name __MODULE__
+  @spec start_link(list) :: :ignore | {:error, list} | {:ok, pid}
+  def start_link(initial_stack) do
+    GenServer.start_link(__MODULE__, initial_stack, name: @name)
+  end
+
+  def get do
+    GenServer.call(@name, {:get})
+  end
+
+  def update(new_stack) do
+    GenServer.cast(@name, {:update, new_stack})
+  end
+
+  ## Server Implementation
+
+  def init(initial_stack) do
+    {:ok, initial_stack}
+  end
+
+  def handle_call({:get}, _from, stack) do
+    {:reply, stack, stack}
+  end
+
+  def handle_cast({:update, new_stack}, _stack) do
+    {:noreply, new_stack}
+  end
+end
